4ee299b6a5a33c148dd8a38c6254f95ec17c8af0
LMCMLJ <mjcalvey2@gmail.com>
2016-10-03 15:57:35 +0100

    Swaps balance method for attribute reader

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,12 @@
 class Oystercard
+
+  attr_reader :balance
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
     @balance += amount
-  end
-
-  def balance
-    @balance
   end
 end
