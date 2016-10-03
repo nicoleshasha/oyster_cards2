622cbb3dc98299794ddbffc5b5c9817f9a28a5f0
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-03 16:20:49 +0100

    Add maximum balance constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize(balance = 5)
     @balance = balance
   end
 
   def top_up(amount)
-    raise("Maximum balance is £90") if (@balance + amount) >= 90 
+    raise("Maximum balance is £#{MAXIMUM_BALANCE}") if (@balance + amount) >= MAXIMUM_BALANCE
     @balance += amount
   end
 
