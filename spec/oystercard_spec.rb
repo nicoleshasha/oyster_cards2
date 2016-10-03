fb6d3055377b9bc12f1cff61f4ab3c5080102860
LMCMLJ <mjcalvey2@gmail.com>
2016-10-03 15:49:07 +0100

    Adds tests for balance and top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,21 @@
+require 'oystercard'
+
 describe Oystercard do
 
+  before :each do
+    @oystercard = Oystercard.new
+  end
+
+  describe '#top_up' do
+    it 'Adds to the balance of the instance' do
+      @oystercard.top_up(10)
+      expect(@oystercard.balance).to eq(10)
+    end
+  end
+
+  describe '#balance' do
+    it 'Returns the balance on the card' do
+      expect(@oystercard.balance).to eq(0)
+    end
+  end
 end
