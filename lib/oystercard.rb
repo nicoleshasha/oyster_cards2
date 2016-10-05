80dc643bcfdeffe9a890995f1703e23b38549dc3
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-05 11:40:48 +0100

    refractored code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,35 +1,28 @@
 class Oystercard
-  attr_reader :balance, :limit, :in_use, :stations, :station, :journey
+  attr_reader :balance, :in_use, :stations, :station, :journey
   DEFAULT_BALANCE = 5
   BALANCE_LIMIT = 90
+  MINIMUM_FARE = 1
 
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
-    @limit = BALANCE_LIMIT
     @in_use = false
-    @stations = ""
   end
 
   def top_up(amount)
-    amount = amount.to_i
-    fail "Total balance should not be more than 90" if (@balance + amount) > @limit
+    fail "Total balance should not be more than 90" if (@balance + amount) > BALANCE_LIMIT
     @balance += amount
   end
 
-  def touch_in
-    if @balance < 1
-      raise "Not enough funds"
-    else
-      @in_use = true
-      @stations = :awesome_coffwee
-    end
+  def touch_in(entry_station)
+    raise "Not enough funds" if @balance < 1
+    @in_use = true
   end
 
-  def touch_out
+  def touch_out(exit_station)
     @in_use = false
-    @stations = ""
-    deduct_balance(1)
+    deduct_balance(MINIMUM_FARE)
   end
 
   def in_journey?
@@ -38,7 +31,6 @@ class Oystercard
 
 
 def deduct_balance(amount)
-  amount = amount.to_i
   @balance -= amount
 end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard) {described_class.new}
+  let(:station) {double :station}
 
   describe "#balance" do
 
@@ -28,30 +29,27 @@ describe Oystercard do
         expect{oystercard.top_up(16)}.to change {subject.balance}.by 16
       end
 
-      it "sets limit for oyster at 90" do
-        expect(oystercard.balance).to be <= oystercard.limit
-      end
       it "raises and error if balance is more than 90" do
-        amount = 100
+          amount = 100
           expect{oystercard.top_up(amount)}.to raise_error "Total balance should not be more than 90"
       end
 end
 
 describe '#touch_in' do
   it "allows a user to touch in at a station" do
-    oystercard.touch_in
+    oystercard.touch_in(station)
     expect(oystercard.in_use).to eq true
   end
 
   it  "tests to see if passenger is in journey" do
-    oystercard.touch_in
+    oystercard.touch_in(station)
     oystercard.in_journey?
     expect(oystercard.in_use).to eq true
 end
 
   it "does not allow to touch in when balance is less than £1" do
     oyster1 = Oystercard.new(0)
-    expect{oyster1.touch_in}.to raise_error "Not enough funds"
+    expect{oyster1.touch_in(station)}.to raise_error "Not enough funds"
   end
 
   it "tests to see if the origin station is stored" do
@@ -68,13 +66,13 @@ end
 
 describe '#touch_out' do
   it "allows a user to touch out at a station" do
-    oystercard.touch_in
-    oystercard.touch_out
+    oystercard.touch_in(station)
+    oystercard.touch_out(station)
     expect(oystercard.in_use).to eq false
   end
 
   it "uses the deduct method to deduct the minimum fare when you touch out" do
-    expect{oystercard.touch_out}.to change {subject.balance}.by -1
+    expect{oystercard.touch_out(station)}.to change {subject.balance}.by -1
   end
 end
 
